a274dcea5c04da44c5dafa33a84d922007510462
Richard Dennis <richard.dennis@glasgow.ac.uk>
2018-10-03 21:44:12 +0100

    Added additional test

--- test/example.jl
+++ test/example.jl
@@ -7,6 +7,7 @@ function test_smolyak_approx()
   d  = 5  # Set the number of dimensions
   mu = 3  # Set the level of approximation
 
+  g, m =  smolyak_grid(legendre_gauss_lobatto,d,mu)
   grid, multi_ind = smolyak_grid(chebyshev_gauss_lobatto,d,mu)  # Construct the Smolyak grid and the multi index
 
   # An arbitrary test function

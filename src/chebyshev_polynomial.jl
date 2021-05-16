09c182743b5fd27699c23b3b20ed84469f55a480
RJDennis <richard.dennis@glasgow.ac.uk>
2021-05-16 03:32:02 +0100

    Efficiency tweeks

--- src/chebyshev_polynomial.jl
+++ src/chebyshev_polynomial.jl
@@ -2,9 +2,9 @@ function chebyshev_polynomial(order::S,x::Array{T,1}) where {T<:AbstractFloat,S<
 
   polynomial      = Array{T}(undef,length(x),order+1)
   polynomial[:,1] = ones(T,length(x))
-
+  
   for i = 2:order+1
-	  for j = 1:length(x)
+      for j = 1:length(x)
       if i == 2
         polynomial[j,i] = x[j]
       else
@@ -12,7 +12,23 @@ function chebyshev_polynomial(order::S,x::Array{T,1}) where {T<:AbstractFloat,S<
       end
     end
   end
-
+  
   return polynomial
+  
+end
 
+function chebyshev_polynomial(order::S,x::T) where {T<:AbstractFloat,S<:Integer}
+
+  polynomial = ones(T,1,order+1)
+  
+  for i = 2:order+1
+    if i == 2
+      polynomial[1,i] = x
+    else
+      polynomial[1,i] = 2*x*polynomial[1,i-1]-polynomial[1,i-2]
+    end
+  end
+  
+  return polynomial
+  
 end

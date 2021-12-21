c85a201897598695e20c3057ecd02b4aadad6daa
RJDennis <richard.dennis@glasgow.ac.uk>
2021-12-21 00:54:00 +0000

    Update chebyshev_polynomial_derivative.jl

--- src/chebyshev_polynomial_derivative.jl
+++ src/chebyshev_polynomial_derivative.jl
@@ -8,7 +8,7 @@ function chebyshev_polynomial_derivative(order::S,x::R) where {R<:Number,S<:Inte
   for i = 2:order+1
     if i == 2
       polynomial[i] = x
-      poly_deriv[i] = one(T)
+      poly_deriv[i] = one(R)
     else
       polynomial[i] = 2*x*polynomial[i-1]-polynomial[i-2]
       poly_deriv[i] = 2*polynomial[i-1]+2*x*poly_deriv[i-1]-poly_deriv[i-2]

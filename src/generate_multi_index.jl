c0f7345c12dc61c95f80207383e55f33138e8b12
RJDennis <richard.dennis@glasgow.ac.uk>
2022-03-20 19:13:42 +0000

    Improved memory efficiency

--- src/generate_multi_index.jl
+++ src/generate_multi_index.jl
@@ -9,20 +9,21 @@ function generate_multi_index(d::S,mu::Array{S,1}) where {S<:Integer}
   max_mu = maximum(mu)
 
   w = Tuple(repeat([max_mu+1],inner = d))
-  j = 1
+  pos = 0
   @inbounds for i = 2:(max_mu+1)^d
     candidate_index = Tuple(CartesianIndices(w)[i])
     if sum(candidate_index) <= d+max_mu && sum(candidate_index .<= mu.+1) == d
-      #if j < nt
-        j += 1
-        multi_index[j,:] = [candidate_index...]
-      #else # handles case where nt is under-estimated (this branch is not used?)
-      #  multi_index = [multi_index; collect(candidate_index)']
-      #end
+        pos += 1
+      if pos > nt # handles the case where nt is under-estimated
+        multi_index = [multi_index; collect(candidate_index)']
+      else
+        multi_index[pos,:] .= candidate_index
+      end
     end
   end
-  if j < nt # handles case where nt is over-estimated
-    multi_index = multi_index[1:j,:]
+
+  if pos < nt # handles case where nt is over-estimated
+    multi_index = multi_index[1:pos,:]
   end
 
   return multi_index
@@ -33,46 +34,46 @@ end
 
 function generate_multi_index(d::S,mu::S) where {S<:Integer}
 
-  nt = num_terms(mu,d)
-  multi_index      = Array{S,2}(undef,nt,d)
-  multi_index[1,:] = ones(S,1,d)
-
-  w = Tuple(repeat([mu+1],inner = d))
-  j = 1
-  @inbounds for i = 2:(mu+1)^d
-    candidate_index = Tuple(CartesianIndices(w)[i])
-    if sum(candidate_index) <= d+mu
-      j += 1
-      multi_index[j,:] = [candidate_index...]
-    end
+  if d < 1
+    error("d must be positive")
   end
 
-  return multi_index
-
-end
-
-# Following function computes the number of terms in the multi-index for the 
-# isotropic case (it also computes the number of terms in a complete 
-# polynominal based on the order and the number of dimensions.
-
-function num_terms(order::S,d::S) where {S <: Integer}
+  if mu < 0
+    error("mu must be non-negative")
+  end
 
   if d == 1
-    return order+1
+    multi_index = zeros(mu+1)
+    for i = 1:mu+1
+      multi_index[i] = i
+    end
+    return multi_index
   else
-    return div(num_terms(order,d-1)*(order+d),d)
+    multi_index_base = generate_multi_index(d-1,mu)
+    N = size(multi_index_base,1)
+    multi_index = zeros(S,N*(mu+1),d)
+    pos = 0
+    @inbounds @views for j = 1:N
+      for i = 1:mu+1
+        if sum(multi_index_base[j,:]) + i <= d+mu
+          pos += 1
+          multi_index[pos,2:d] .= multi_index_base[j,:]
+          multi_index[pos,1] = i
+        end
+      end
+    end
+    return multi_index[1:pos,:]
   end
-
-end
-
-# The following function is a poor approximation to the number of terms in 
+end    
+  
+# The following function is a poor approximation to the number of terms in
 # the multi-index for the ansiotropic case.
 
 function num_terms(order::Array{S,1},d::S) where {S <: Integer}
 
   max_mu = maximum(order)
   nt = num_terms(max_mu,d) # Deliberate over-estimate of the number of terms
-  
+    
   return nt
-
+  
 end
--- src/smolyak_weights.jl
+++ src/smolyak_weights.jl
@@ -1,39 +1,39 @@
 function smolyak_weights(y::AbstractArray{T,1},nodes::Union{Array{T,1},Array{T,2}},multi_index::Array{S,2}) where {T<:AbstractFloat,S<:Integer}
 
   interpolation_matrix = zeros(size(nodes,1),size(nodes,1))
-  
+
   unique_multi_index = sort(unique(multi_index))
   unique_orders      = m_i(unique_multi_index).-1
-  
+
   # Below we do the following things:
-  
+
   #   Generate the polynomial terms for each order
   #   Generate the unique polynomial terms introduced at each higher order
   #   Combine the polynomial terms to construct a row of the interpolation matrix
   #   Iterate over the nodes, doing the above for steps at each iteration, to compute all rows of the interpolation matrix
-  
+
   base_polynomials        = Array{Array{T,2},1}(undef,length(unique_orders))
   unique_base_polynomials = Array{Array{T,2},1}(undef,length(unique_orders))
-  
+
   @inbounds for k = 1:size(nodes,1)
-  
+
     # Construct the base polynomials
-  
+
     for i = 1:length(unique_orders)
       base_polynomials[i] = chebyshev_polynomial(unique_orders[i],nodes[k,:])
     end
-  
+
     # Compute the unique polynomial terms from the base polynomials
-  
+
     for i = length(unique_orders):-1:2
       unique_base_polynomials[i] = base_polynomials[i][:,size(base_polynomials[i-1],2)+1:end]
     end
     unique_base_polynomials[1] = base_polynomials[1]
-  
+
     # Construct a row of the interplation matrix
-  
+
     l = 1
-    @inbounds for j = 1:size(multi_index,1)
+    @inbounds @views for j = 1:size(multi_index,1)
       new_polynomials = unique_base_polynomials[multi_index[j,1]][1,:]
       for i = 2:size(multi_index,2)
         new_polynomials = kron(new_polynomials,unique_base_polynomials[multi_index[j,i]][i,:])
@@ -42,67 +42,67 @@ function smolyak_weights(y::AbstractArray{T,1},nodes::Union{Array{T,1},Array{T,2
       interpolation_matrix[k,l:l+m-1] = new_polynomials
       l += m
     end
-  
+
   end
-  
-  weights = interpolation_matrix\y 
-  
+
+  weights = interpolation_matrix\y
+
   return weights
-  
+
 end
-  
+
 function smolyak_weights(y::AbstractArray{T,1},nodes::Union{Array{T,1},Array{T,2}},multi_index::Array{S,2},domain::Union{Array{T,1},Array{T,2}}) where {T<:AbstractFloat,S<:Integer}
-  
+
   # Normalize nodes to the [-1.0 1.0] interval
-  
+
   d = size(multi_index,2)
   nodes = copy(nodes)
   for i = 1:d
     nodes[:,i] = normalize_node(nodes[:,i],domain[:,i])
   end
-  
+
   weights = smolyak_weights(y,nodes,multi_index)
-  
+
   return weights
-  
+
 end
-  
+
 function smolyak_weights_threaded(y::AbstractArray{T,1},nodes::Union{Array{T,1},Array{T,2}},multi_index::Array{S,2}) where {T<:AbstractFloat,S<:Integer}
-  
+
   interpolation_matrix = zeros(size(nodes,1),size(nodes,1))
-  
+
   unique_multi_index = sort(unique(multi_index))
   unique_orders      = m_i(unique_multi_index).-1
-  
+
   # Below we do the following things:
-  
+
   #   Generate the polynomial terms for each order
   #   Generate the unique polynomial terms introduced at each higher order
   #   Combine the polynomial terms to construct a row of the interpolation matrix
   #   Iterate over the nodes, doing the above for steps at each iteration, to compute all rows of the interpolation matrix
-  
+
   @inbounds @sync @qthreads for k = 1:size(nodes,1)
-  
+
     base_polynomials        = Array{Array{T,2},1}(undef,length(unique_orders))
     unique_base_polynomials = Array{Array{T,2},1}(undef,length(unique_orders))
-    
+
     # Construct the base polynomials
-  
+
     for i = 1:length(unique_orders)
       base_polynomials[i] = chebyshev_polynomial(unique_orders[i],nodes[k,:])
     end
-  
+
     # Compute the unique polynomial terms from the base polynomials
-  
+
     for i = length(unique_orders):-1:2
       unique_base_polynomials[i] = base_polynomials[i][:,size(base_polynomials[i-1],2)+1:end]
     end
     unique_base_polynomials[1] = base_polynomials[1]
-  
+
     # Construct a row of the interplation matrix
-  
+
     l = 1
-    @inbounds for j = 1:size(multi_index,1)
+    @inbounds @views for j = 1:size(multi_index,1)
       new_polynomials = unique_base_polynomials[multi_index[j,1]][1,:]
       for i = 2:size(multi_index,2)
         new_polynomials = kron(new_polynomials,unique_base_polynomials[multi_index[j,i]][i,:])
@@ -111,67 +111,67 @@ function smolyak_weights_threaded(y::AbstractArray{T,1},nodes::Union{Array{T,1},
       interpolation_matrix[k,l:l+m-1] = new_polynomials
       l += m
     end
-  
+
   end
-  
-  weights = interpolation_matrix\y 
-  
+
+  weights = interpolation_matrix\y
+
   return weights
-  
+
 end
-    
+
 function smolyak_weights_threaded(y::AbstractArray{T,1},nodes::Union{Array{T,1},Array{T,2}},multi_index::Array{S,2},domain::Union{Array{T,1},Array{T,2}}) where {T<:AbstractFloat,S<:Integer}
-  
+
   # Normalize nodes to the [-1.0 1.0] interval
-  
+
   d = size(multi_index,2)
   nodes = copy(nodes)
   for i = 1:d
     nodes[:,i] = normalize_node(nodes[:,i],domain[:,i])
   end
-  
+
   weights = smolyak_weights_threaded(y,nodes,multi_index)
-  
+
   return weights
-  
+
 end
-  
+
 function smolyak_inverse_interpolation_matrix(nodes::Union{Array{T,1},Array{T,2}},multi_index::Array{S,2}) where {T<:AbstractFloat,S<:Integer}
-  
+
   interpolation_matrix = zeros(size(nodes,1),size(nodes,1))
-  
+
   unique_multi_index = sort(unique(multi_index))
   unique_orders      = m_i(unique_multi_index).-1
-  
+
   # Below we do the following things:
-  
+
   #   Generate the polynomial terms for each order
   #   Generate the unique polynomial terms introduced at each higher order
   #   Combine the polynomial terms to construct a row of the interpolation matrix
   #   Iterate over the nodes, doing the above for steps at each iteration, to compute all rows of the interpolation matrix
-  
+
   base_polynomials        = Array{Array{T,2},1}(undef,length(unique_orders))
   unique_base_polynomials = Array{Array{T,2},1}(undef,length(unique_orders))
-  
+
   @inbounds for k = 1:size(nodes,1)
-  
+
     # Construct the base polynomials
-  
+
     for i = 1:length(unique_orders)
       base_polynomials[i] = chebyshev_polynomial(unique_orders[i],nodes[k,:])
     end
-  
+
     # Compute the unique polynomial terms from the base polynomials
-  
+
     for i = length(unique_orders):-1:2
       unique_base_polynomials[i] = base_polynomials[i][:,size(base_polynomials[i-1],2)+1:end]
     end
     unique_base_polynomials[1] = base_polynomials[1]
-  
+
     # Construct the first row of the interplation matrix
-  
+
     l = 1
-    @inbounds for j = 1:size(multi_index,1)
+    @inbounds @views for j = 1:size(multi_index,1)
       new_polynomials = unique_base_polynomials[multi_index[j,1]][1,:]
       for i = 2:size(multi_index,2)
         new_polynomials = kron(new_polynomials,unique_base_polynomials[multi_index[j,i]][i,:])
@@ -180,47 +180,47 @@ function smolyak_inverse_interpolation_matrix(nodes::Union{Array{T,1},Array{T,2}
       interpolation_matrix[k,l:l+m-1] = new_polynomials
       l += m
     end
-  
+
   end
-  
+
   inverse_interpolation_matrix = inv(interpolation_matrix)
-  
+
   return inverse_interpolation_matrix
-  
+
 end
-  
+
 function smolyak_inverse_interpolation_matrix(nodes::Union{Array{T,1},Array{T,2}},multi_index::Array{S,2},domain::Union{Array{T,1},Array{T,2}}) where {T<:AbstractFloat,S<:Integer}
-  
+
   # Normalize nodes to the [-1.0 1.0] interval
-  
+
   d = size(multi_index,2)
   nodes = copy(nodes)
   for i = 1:d
     nodes[:,i] = normalize_node(nodes[:,i],domain[:,i])
   end
-  
+
   inverse_interpolation_matrix = smolyak_inverse_interpolation_matrix(nodes,multi_index)
-  
+
   return inverse_interpolation_matrix
-  
+
 end
-  
+
 function smolyak_inverse_interpolation_matrix_threaded(nodes::Union{Array{T,1},Array{T,2}},multi_index::Array{S,2}) where {T<:AbstractFloat,S<:Integer}
-  
+
   interpolation_matrix = zeros(size(nodes,1),size(nodes,1))
-  
+
   unique_multi_index = sort(unique(multi_index))
   unique_orders      = m_i(unique_multi_index).-1
-  
+
   # Below we do the following things:
-  
+
   #   Generate the polynomial terms for each order
   #   Generate the unique polynomial terms introduced at each higher order
   #   Combine the polynomial terms to construct a row of the interpolation matrix
   #   Iterate over the nodes, doing the above for steps at each iteration, to compute all rows of the interpolation matrix
-  
+
   @inbounds @sync @qthreads for k = 1:size(nodes,1)
-  
+
     base_polynomials        = Array{Array{T,2},1}(undef,length(unique_orders))
     unique_base_polynomials = Array{Array{T,2},1}(undef,length(unique_orders))
 
@@ -229,18 +229,18 @@ function smolyak_inverse_interpolation_matrix_threaded(nodes::Union{Array{T,1},A
     for i = 1:length(unique_orders)
       base_polynomials[i] = chebyshev_polynomial(unique_orders[i],nodes[k,:])
     end
-  
+
     # Compute the unique polynomial terms from the base polynomials
-  
+
     for i = length(unique_orders):-1:2
       unique_base_polynomials[i] = base_polynomials[i][:,size(base_polynomials[i-1],2)+1:end]
     end
     unique_base_polynomials[1] = base_polynomials[1]
-  
+
     # Construct the first row of the interplation matrix
-  
+
     l = 1
-    @inbounds for j = 1:size(multi_index,1)
+    @inbounds @views for j = 1:size(multi_index,1)
       new_polynomials = unique_base_polynomials[multi_index[j,1]][1,:]
       for i = 2:size(multi_index,2)
         new_polynomials = kron(new_polynomials,unique_base_polynomials[multi_index[j,i]][i,:])
@@ -249,35 +249,35 @@ function smolyak_inverse_interpolation_matrix_threaded(nodes::Union{Array{T,1},A
       interpolation_matrix[k,l:l+m-1] = new_polynomials
       l += m
     end
-  
+
   end
-  
+
   inverse_interpolation_matrix = inv(interpolation_matrix)
-  
+
   return inverse_interpolation_matrix
-  
+
 end
-  
+
 function smolyak_inverse_interpolation_matrix_threaded(nodes::Union{Array{T,1},Array{T,2}},multi_index::Array{S,2},domain::Union{Array{T,1},Array{T,2}}) where {T<:AbstractFloat,S<:Integer}
-  
+
   # Normalize nodes to the [-1.0 1.0] interval
-  
+
   d = size(multi_index,2)
   nodes = copy(nodes)
   for i = 1:d
     nodes[:,i] = normalize_node(nodes[:,i],domain[:,i])
   end
-  
+
   inverse_interpolation_matrix = smolyak_inverse_interpolation_matrix_threaded(nodes,multi_index)
-  
+
   return inverse_interpolation_matrix
-  
+
 end
-  
+
 function smolyak_weights(y::AbstractArray{T,1},inverse_interpolation_matrix::Array{T,2}) where {T<:AbstractFloat}
-  
+
   weights = inverse_interpolation_matrix*y
-  
+
   return weights
-  
+
 end
